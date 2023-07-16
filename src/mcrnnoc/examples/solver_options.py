032d0025ec8e9f59ba7196336c7394dd1b847a88
Johannes Milz <johannes.milz@isye.gatech.edu>
2023-07-16 10:35:39 -0400

    Minor updates.

--- src/mcrnnoc/examples/solver_options.py
+++ src/mcrnnoc/examples/solver_options.py
@@ -1,17 +1,21 @@
 import numpy as np
 from fw4pde.stepsize import DemyanovRubinovOptimalStepSize
+from fw4pde.stepsize import DunnScalingStepSize
+from fw4pde.stepsize import DemyanovRubinovAdaptiveStepSize
+
 
 class SolverOptions(object):
 
     def __init__(self):
 
-        maxiter = 30
+        maxiter = 50
         gtol = 1e-8
         ftol = -np.inf
 
         self._options = {"maxiter": maxiter, "gtol": gtol, "ftol": ftol}
+        self._stepsize = DunnScalingStepSize()
+        self._stepsize = DemyanovRubinovAdaptiveStepSize()
         self._stepsize = DemyanovRubinovOptimalStepSize()
-
 
     @property
     def options(self):
