d1d52ad67a33cc39f627cebbe20081e78a8b6864
Johannes Milz <johannes.milz@isye.gatech.edu>
2023-07-16 17:53:59 -0400

    Added options for colorbar.

--- src/mcrnnoc/examples/bilinear/plot_control.py
+++ src/mcrnnoc/examples/bilinear/plot_control.py
@@ -11,6 +11,12 @@ input_dir = "output/" + sys.argv[1]
 n = input_dir.split("_n=")[-1].split(".")[0].split("_")[0]
 n = int(n)
 
+colorbar = 1
+
+if len(sys.argv) > 2:
+  colorbar = int(sys.argv[2])
+end
+
 input_filename = input_dir
 u_vec = np.loadtxt(input_filename + ".txt")
 
@@ -20,7 +26,10 @@ u = Function(U)
 u.vector()[:] = u_vec
 
 p = plot(u, wireframe=False, cmap=cm.coolwarm)
-# https://stackoverflow.com/questions/18195758/set-matplotlib-colorbar-size-to-match-graph
-plt.colorbar(p, fraction=0.046, pad=0.04)
+if colorbar == 1:
+  # https://stackoverflow.com/questions/18195758/set-matplotlib-colorbar-size-to-match-graph
+  plt.colorbar(p, fraction=0.046, pad=0.04)
+end
+
 plt.tight_layout()
 plt.savefig(input_dir + ".pdf")
