85fcc333d9b9011f681ff4fbe1000403525f5a83
Johannes Milz <johannes.milz@isye.gatech.edu>
2023-07-22 15:31:17 -0400

    Minor updates.

--- src/mcrnnoc/examples/bilinear/risk_neutral/plot_experiment.sh
+++ src/mcrnnoc/examples/bilinear/risk_neutral/plot_experiment.sh
@@ -1,1 +1,3 @@
 python plot_experiment.py output/Experiments/Monte_Carlo_Rate_Synthetic_22-Jul-2023-11-07-59
+python plot_experiment.py output/Experiments/Monte_Carlo_Rate_Synthetic_22-Jul-2023-14-29-11
+python plot_experiment.py output/Experiments/Monte_Carlo_Rate_Synthetic_22-Jul-2023-14-30-58
--- src/mcrnnoc/examples/bilinear/risk_neutral/test_simulate_experiment.sh
+++ src/mcrnnoc/examples/bilinear/risk_neutral/test_simulate_experiment.sh
@@ -2,8 +2,8 @@
 date=$(date '+%d-%b-%Y-%H-%M-%S')
 source ../../simulation_data.sh
 
-experiment="Monte_Carlo_Rate_Test"
 experiment="Monte_Carlo_Rate_Synthetic"
+experiment="Monte_Carlo_Rate_Test"
 Nref="8"
 mpiexec -n 2 python simulate_experiment.py $date $experiment $Nref
 
