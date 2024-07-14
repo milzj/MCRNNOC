cb941966d5d144649399d07bd427c566a126ab00
Johannes Milz <johannes.milz@isye.gatech.edu>
2024-07-14 13:12:25 -0400

    .

--- src/mcrnnoc/examples/bilinear/plot_control.py
+++ src/mcrnnoc/examples/bilinear/plot_control.py
@@ -8,14 +8,13 @@ cmap_blue_orange = LinearSegmentedColormap.from_list(name="cmap_BlueOrange",
                                           colors =["tab:blue", "lightgrey", "tab:orange"],
                                             N=256)
 
-from mcrnnoc.stats import figure_style
+#from mcrnnoc.stats import figure_style
 
 import sys
 
 input_dir = "output/" + sys.argv[1]
 n = input_dir.split("_n=")[-1].split(".")[0].split("_")[0]
 n = int(n)
-
 colorbar = 1
 
 if len(sys.argv) > 2:
@@ -29,7 +28,6 @@ mesh = UnitSquareMesh(n, n)
 U = FunctionSpace(mesh, "DG", 0)
 u = Function(U)
 u.vector()[:] = u_vec
-
 p = plot(u, wireframe=False, cmap=cmap_blue_orange)
 if colorbar == 1:
   # https://stackoverflow.com/questions/18195758/set-matplotlib-colorbar-size-to-match-graph
