46413eeb21ef7afffc2d1a43cb653bcd826608c3
Johannes Milz <johannes.milz@isye.gatech.edu>
2024-07-17 19:30:20 -0400

    Updated experiments.py

--- src/mcrnnoc/examples/bilinear/risk_neutral/plot_experiment.sh
+++ src/mcrnnoc/examples/bilinear/risk_neutral/plot_experiment.sh
@@ -1,2 +1,2 @@
-python plot_experiment.py output/Experiments/Monte_Carlo_Rate_16-Jul-2024-09-55-54 output/Reference_Simulation_n=64_N=4096_date=16-Jul-2024-11-05-09
-python plot_experiment.py output/Experiments/Monte_Carlo_Rate_16-Jul-2024-09-55-54
+python plot_experiment.py output/Experiments/Monte_Carlo_Rate_17-Jul-2024-12-58-49 output/Reference_Simulation_n=64_N=4096_date=17-Jul-2024-12-58-49
+python plot_experiment.py output/Experiments/Monte_Carlo_Rate_17-Jul-2024-12-57-50 output/Reference_Simulation_n=64_N=4096_date=17-Jul-2024-12-57-50
