6ec13694de4b71000d897e196a04313e5b0cfa9e
Johannes Milz <johannes.milz@isye.gatech.edu>
2024-07-18 09:27:17 -0400

    Checking if latex is available.

--- src/mcrnnoc/stats/figure_style.py
+++ src/mcrnnoc/stats/figure_style.py
@@ -1,23 +1,25 @@
-import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from cycler import cycler
+import shutil
+from matplotlib import rcParams
+import shutil
 
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
 
-fontsize = 16.5
-fontsize = 14.5
-fontsize = 12.5
-linewidth = .75
+fontsize = 14
+linewidth = 1.
 
 color_styles = 2*['k']
 marker_styles = ['d', 'o', 'p', 's', '<']
 line_styles = ['-', '--', "-.", ':', '--']
 line_styles = ['-', "-."]
 
+if shutil.which('latex'):
+    plt.rcParams['text.usetex'] = True
+    plt.rcParams["text.latex.preamble"] = r"\usepackage{amsfonts}"
+
 plt.rcParams.update({
-	"text.usetex": True,
-	"text.latex.preamble": r"\usepackage{amsfonts}",
 	"font.family": "serif",
 	"font.serif": "Computer Modern Roman",
 	"font.monospace": "Computer Modern Typewriter",
@@ -32,7 +34,6 @@ plt.rcParams.update({
 	"axes.prop_cycle": (cycler('color', color_styles) +
 		cycler('linestyle', line_styles))
 	})
-
 
 def latex_float(f, signif = 1):
 	float_str = "{0:.2g}".format(f)
